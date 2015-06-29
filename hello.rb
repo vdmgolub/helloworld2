ad70c312cb1238359a4e06d997922e6b52137b0b
Vadim Golub <vadim@rightscale.com>
2015-06-29 14:10:17 +0300

    Refactor Greeter with say method

--- hello.rb
+++ hello.rb
@@ -1,9 +1,14 @@
 class Greeter
   def hello(name)
-    puts "Hello #{name}!"
+    say "Hello #{name}!"
   end
 
   def bye(name)
     puts "Bye-bye"
   end
+
+  def say(message)
+    puts message
+  end
+  protected :say
 end
